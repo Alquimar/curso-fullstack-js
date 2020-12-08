bd5c521aa55602a2ef8775f75f07d8caa717931f
Alquimar Almeida <alquimars@gmail.com>
2020-12-08 13:48:15 -0300

    Add accounts.auth.test

--- backend/accounts-service/__tests__/accounts.test.ts
+++ backend/accounts-service/__tests__/accounts.test.ts
@@ -41,6 +41,49 @@ describe('Testando rotas do accounts', () => {
         expect(resultado.status).toEqual(422);
     });
 
+    it('PATCH /accounts/:id - Deve retornar statusCode 200', async () => {
+        const payload = {
+            name: 'Alquimar Almeida',
+            email: 'alquimars@gmail.com',
+            password: '123456789'
+        };
+
+        const resultado = await request(app)
+            .patch('/accounts/1')
+            .send(payload);
+        
+        expect(resultado.status).toEqual(200);
+        expect(resultado.body.id).toEqual(1);
+    });
+
+    it('PATCH /accounts/:id - Deve retornar statusCode 400', async () => {
+        const payload = {
+            name: 'Alquimar Almeida',
+            email: 'alquimars@gmail.com',
+            password: '123456789'
+        };
+
+        const resultado = await request(app)
+            .patch('/accounts/abc')
+            .send(payload);
+        
+        expect(resultado.status).toEqual(400);
+    });
+
+    it('PATCH /accounts/:id - Deve retornar statusCode 404', async () => {
+        const payload = {
+            name: 'Alquimar Almeida',
+            email: 'alquimars@gmail.com',
+            password: '123456789'
+        };
+
+        const resultado = await request(app)
+            .patch('/accounts/-1')
+            .send(payload);
+        
+        expect(resultado.status).toEqual(404);
+    });
+
     it('GET /accounts/:id - Deve retornar statusCode 200', async () => {
         const resultado = await request(app)
             .get('/accounts/1');
