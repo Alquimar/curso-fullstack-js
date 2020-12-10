1be8ddb4bc1ef452074236b07255124ae5429c48
Alquimar Almeida <alquimars@gmail.com>
2020-12-10 12:25:39 -0300

    Refactoring common codes in the module '__commons__'

--- backend/accounts-service/src/routes/accounts.ts
+++ backend/accounts-service/src/routes/accounts.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import accountsController from '../controllers/accounts'
-import { validateAccountSchema, validateLoginSchema, validateUpdateAccountSchema, validateAuth } from './middlewares';
+import { validateAccountSchema, validateLoginSchema, validateUpdateAccountSchema } from './middlewares';
+import middlewareCommons from 'ms-commons/api/routes/middlewares';
 
 const router = Router();
 
-router.get('/accounts/', validateAuth, accountsController.getAccounts);
-router.get('/accounts/:id', validateAuth, accountsController.getAccount);
-router.patch('/accounts/:id', validateAuth, validateUpdateAccountSchema, accountsController.setAccount);
+router.get('/accounts/', middlewareCommons.validateAuth, accountsController.getAccounts);
+router.get('/accounts/:id', middlewareCommons.validateAuth, accountsController.getAccount);
+router.patch('/accounts/:id', middlewareCommons.validateAuth, validateUpdateAccountSchema, accountsController.setAccount);
 router.post('/accounts/', validateAccountSchema, accountsController.addAccount);
 router.post('/accounts/login', validateLoginSchema, accountsController.loginAccount);
 router.post('/accounts/logout', accountsController.logoutAccount);
--- backend/accounts-service/src/routes/middlewares.ts
+++ backend/accounts-service/src/routes/middlewares.ts
@@ -1,46 +1,17 @@
 import { Request, Response } from 'express';
-import Joi from 'joi';
 import { accountSchema, loginSchema, accountUpdateSchema } from '../models/accountSchemas';
-import auth from '../auth';
-
-function validateSchema(schema: Joi.ObjectSchema<any>, req: Request, res: Response, next: any) {
-    const { error } = schema.validate(req.body);
-    if (error == null) return next();
-
-    const { details } = error;
-    const message = details.map(item => item.message).join(',');
-
-    console.log(`validateSchema: ${message}`);
-    res.status(422).end();
-}
+import middlewareCommons from 'ms-commons/api/routes/middlewares';
 
 function validateAccountSchema(req: Request, res: Response, next: any) {
-    return validateSchema(accountSchema, req, res, next);
+    return middlewareCommons.validateSchema(accountSchema, req, res, next);
 }
 
 function validateUpdateAccountSchema(req: Request, res: Response, next: any) {
-    return validateSchema(accountUpdateSchema, req, res, next);
+    return middlewareCommons.validateSchema(accountUpdateSchema, req, res, next);
 }
 
 function validateLoginSchema(req: Request, res: Response, next: any) {
-    return validateSchema(loginSchema, req, res, next);
+    return middlewareCommons.validateSchema(loginSchema, req, res, next);
 }
 
-async function validateAuth(req: Request, res: Response, next: any) {
-    try {
-        const token = req.headers['x-access-token'] as string;
-        if (!token) return res.status(401).end();
-
-        const payload = await auth.verify(token);
-        if (!payload) return res.status(401).end();
-
-        res.locals.payload = payload;
-
-        next();
-    } catch (error) {
-        console.log(`validateAuth: ${error}`);
-        res.status(401).end();
-    }
-}
-
-export { validateAccountSchema, validateLoginSchema, validateUpdateAccountSchema, validateAuth };
+export { validateAccountSchema, validateLoginSchema, validateUpdateAccountSchema };
