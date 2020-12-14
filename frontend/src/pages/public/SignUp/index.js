ef9b695c02622f3299a56d8c9e8e560f9e4c8962
Alquimar Almeida <alquimars@gmail.com>
2020-12-14 16:08:18 -0300

    Final frontend signin and signup

--- frontend/src/pages/public/SignUp/index.js
+++ frontend/src/pages/public/SignUp/index.js
@@ -23,7 +23,43 @@ class SignUp extends React.Component {
                         <BoxForm>
                             <h2>Cadastro</h2>
                             <p>Informe todos os campos para realizar o cadastro.</p>
+                            <Form>
+                                <Form.Group controlId="nomeGroup">
+                                    <Form.Label>Nome:</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Digite seu nome"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="emailGroup">
+                                    <Form.Label>E-mail:</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="Digite seu e-mail"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="dominioGroup">
+                                    <Form.Label>Domínio:</Form.Label>
+                                    <Form.Control
+                                        type="url"
+                                        placeholder="Digite seu domínio"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="senhaGroup">
+                                    <Form.Label>Senha:</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Digite uma senha"
+                                    />
+                                </Form.Group>
+                            </Form>
+                            <Button block variant="primary">
+                                Realizar cadastro
+                            </Button>
                         </BoxForm>
+                        <BoxContent>
+                            <Link className="button" to="/signin">Voltar para o login</Link>
+                        </BoxContent>
                     </Col>
                 </Row>
             </Container>
